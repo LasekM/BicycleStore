33ab01390cafede0d5bfabfb47a97f08c6c71c59
chaoxly <kwak.michal@gmail.com>
2024-06-15 23:03:52 +0200

    Rozdzielenie na BikeController oraz SupplierController, dodanie widoku Search, zmiana "Home" ( index ) oraz css, ukrycie niektórych funkcji przed niezalogowanym użytkownikiem ( admin )

--- BicycleStore/Controllers/BikeController.cs
+++ BicycleStore/Controllers/BikeController.cs
@@ -1,9 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using BicycleStore.Models;
-using System.Collections.Generic;
 using System.Linq;
 using BicycleStore.DbContext;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 
 namespace BicycleStore.Controllers
 {
@@ -18,85 +18,56 @@ namespace BicycleStore.Controllers
 
         public IActionResult Index()
         {
-            var suppliers = _context.Suppliers.ToList();
-            return View(suppliers);
+            var bikes = _context.Bikes.ToList();
+            return View(bikes);
         }
 
         public IActionResult Details(int id)
         {
-            var supplier = _context.Suppliers.FirstOrDefault(s => s.Id == id);
-            if (supplier == null)
+            var bike = _context.Bikes.Include(b => b.Supplier).FirstOrDefault(b => b.Id == id);
+            if (bike == null)
             {
                 return NotFound();
             }
-            var bikes = _context.Bikes.Where(b => b.SupplierID == id).ToList();
-            ViewBag.Bikes = bikes;
-            return View(supplier);
+            return View(bike);
         }
 
-        public IActionResult CreateSupplier()
-        {
-            return View();
-        }
 
-        [HttpPost]
-        public IActionResult CreateSupplier(Supplier supplier)
+        public IActionResult Create(int supplierId)
         {
-            if (ModelState.IsValid)
-            {
-                _context.Suppliers.Add(supplier);
-                _context.SaveChanges();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(supplier);
-        }
-
-        public IActionResult CreateBike()
-        {
+            /*var suppliers = _context.Suppliers.ToList();
+            ViewBag.Suppliers = new SelectList(suppliers, "Id", "Name");
+            return View();*/
             var suppliers = _context.Suppliers.ToList();
             ViewBag.Suppliers = new SelectList(suppliers, "Id", "Name");
+            ViewBag.SelectedSupplierId = supplierId;
             return View();
         }
 
         [HttpPost]
-        public IActionResult CreateBike(Bike bike)
+        public IActionResult Create(Bike bike)
         {
+            /* if (ModelState.IsValid)
+             {
+                 _context.Bikes.Add(bike);
+                 _context.SaveChanges();
+                 return RedirectToAction(nameof(Index));
+             }
+             var suppliers = _context.Suppliers.ToList();
+             ViewBag.Suppliers = new SelectList(suppliers, "Id", "Name");
+             return View(bike);*/
             if (ModelState.IsValid)
             {
                 _context.Bikes.Add(bike);
                 _context.SaveChanges();
-                return RedirectToAction(nameof(Index));
+                return RedirectToAction(nameof(Details), new { id = bike.Id });
             }
             var suppliers = _context.Suppliers.ToList();
             ViewBag.Suppliers = new SelectList(suppliers, "Id", "Name");
             return View(bike);
         }
 
-
-
-        public IActionResult EditSupplier(int id)
-        {
-            var supplier = _context.Suppliers.FirstOrDefault(s => s.Id == id);
-            if (supplier == null)
-            {
-                return NotFound();
-            }
-            return View(supplier);
-        }
-
-        [HttpPost]
-        public IActionResult EditSupplier(Supplier supplier)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Suppliers.Update(supplier);
-                _context.SaveChanges();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(supplier);
-        }
-
-        public IActionResult EditBike(int id)
+        public IActionResult Edit(int id)
         {
             var bike = _context.Bikes.FirstOrDefault(b => b.Id == id);
             if (bike == null)
@@ -104,22 +75,58 @@ namespace BicycleStore.Controllers
                 return NotFound();
             }
             var suppliers = _context.Suppliers.ToList();
-            ViewBag.Suppliers = new SelectList(suppliers, "Id", "Name");
+            ViewBag.Suppliers = new SelectList(suppliers, "Id", "Name", bike.SupplierID);
             return View(bike);
         }
 
         [HttpPost]
-        public IActionResult EditBike(Bike bike)
+        public IActionResult Edit(Bike bike)
         {
             if (ModelState.IsValid)
             {
                 _context.Bikes.Update(bike);
                 _context.SaveChanges();
-                return RedirectToAction(nameof(Details), new { id = bike.SupplierID });
+                return RedirectToAction(nameof(Details), new { id = bike.Id });
             }
             var suppliers = _context.Suppliers.ToList();
-            ViewBag.Suppliers = new SelectList(suppliers, "Id", "Name");
+            ViewBag.Suppliers = new SelectList(suppliers, "Id", "Name", bike.SupplierID);
             return View(bike);
+        }
+
+        [HttpGet]
+        public IActionResult Search(string searchString)
+        {
+            var bikes = from b in _context.Bikes.Include(b => b.Supplier)
+                        select b;
+
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                bikes = bikes.Where(s => s.Model.Contains(searchString));
+            }
+
+            return View(bikes.ToList());
+        }
+
+        [HttpGet]
+        public JsonResult SearchBikes(string searchString)
+        {
+            var bikes = from b in _context.Bikes.Include(b => b.Supplier)
+                        select b;
+
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                searchString = searchString.ToLower();
+                bikes = bikes.Where(s => s.Model.ToLower().Contains(searchString));
+            }
+
+            var result = bikes.Select(b => new {
+                b.Id,
+                b.Model,
+                b.Price,
+                SupplierName = b.Supplier.Name
+            }).ToList();
+
+            return Json(result);
         }
 
 
@@ -129,11 +136,7 @@ namespace BicycleStore.Controllers
 
 
 
-
-
-
-
-        public IActionResult DeleteBike(int id)
+        public IActionResult Delete(int id)
         {
             var bike = _context.Bikes.FirstOrDefault(b => b.Id == id);
             if (bike == null)
@@ -143,8 +146,8 @@ namespace BicycleStore.Controllers
             return View(bike);
         }
 
-        [HttpPost, ActionName("DeleteBikeConfirmed")]
-        public IActionResult DeleteBikeConfirmed(int id)
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
         {
             var bike = _context.Bikes.FirstOrDefault(b => b.Id == id);
             if (bike != null)
@@ -155,37 +158,5 @@ namespace BicycleStore.Controllers
             }
             return NotFound();
         }
-
-
-
-
-
-
-
-        public IActionResult DeleteSupplier(int id)
-        {
-            var supplier = _context.Suppliers.FirstOrDefault(s => s.Id == id);
-            if (supplier == null)
-            {
-                return NotFound();
-            }
-            return View(supplier);
-        }
-
-        [HttpPost, ActionName("DeleteConfirmed")]
-        public IActionResult DeleteSupplierConfirmed(int id)
-        {
-            var supplier = _context.Suppliers.FirstOrDefault(s => s.Id == id);
-            if (supplier != null)
-            {
-                var bikes = _context.Bikes.Where(b => b.SupplierID == id).ToList();
-                _context.Bikes.RemoveRange(bikes);
-                _context.Suppliers.Remove(supplier);
-                _context.SaveChanges();
-                return RedirectToAction(nameof(Index));
-            }
-            return NotFound();
-        }
-
     }
 }
---
+++ BicycleStore/Controllers/SupplierController.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BicycleStore.DbContext;
+using BicycleStore.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BicycleStore.Controllers
+{
+    public class SupplierController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public SupplierController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var suppliers = _context.Suppliers.ToList();
+            return View(suppliers);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var supplier = _context.Suppliers
+                                    .Include(s => s.Bikes) 
+                                    .FirstOrDefault(s => s.Id == id);
+            if (supplier == null)
+            {
+                return NotFound();
+            }
+            return View(supplier);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Supplier supplier)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Suppliers.Add(supplier);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(supplier);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var supplier = _context.Suppliers.FirstOrDefault(s => s.Id == id);
+            if (supplier == null)
+            {
+                return NotFound();
+            }
+            return View(supplier);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Supplier supplier)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Suppliers.Update(supplier);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(supplier);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var supplier = _context.Suppliers.FirstOrDefault(s => s.Id == id);
+            if (supplier == null)
+            {
+                return NotFound();
+            }
+            return View(supplier);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var supplier = _context.Suppliers.FirstOrDefault(s => s.Id == id);
+            if (supplier != null)
+            {
+                _context.Suppliers.Remove(supplier);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return NotFound();
+        }
+
+        // Dodajemy metodę CreateBike
+        public IActionResult CreateBike(int id)
+        {
+            return RedirectToAction("Create", "Bike", new { supplierId = id });
+        }
+    }
+}
