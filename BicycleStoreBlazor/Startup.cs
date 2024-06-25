0bd1634ee387ec75f6fed3e8ac4e49b9256e3a4e
LasekM <poltok00@gmail.com>
2024-06-25 16:07:10 +0200

    Blazor added, not working yet

---
+++ BicycleStoreBlazor/Startup.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BicycleStoreBlazor
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+            services.AddServerSideBlazor();
+            services.AddHttpClient();
+            services.AddControllers();
+
+            services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:5224") });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapBlazorHub();
+                endpoints.MapFallbackToPage("/_Host");
+            });
+        }
+    }
+}
